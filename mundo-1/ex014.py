5e535b25c2879e553f8b19098968f970f1ed34ae
edbsousa <edeilson.sousa@hotmail.com>
2021-11-29 20:20:52 -0300

    Created 14 exercise

---
+++ mundo-1/ex014.py
@@ -0,0 +1,6 @@
+# Exercício 14 – Conversor de Temperaturas
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+c = float(input('Digite a temperatura em graus Celsius: '))
+
+print(f'\n{c}Cº é igual {c * 1.8 + 32}Fº (Fahrenheit)')
