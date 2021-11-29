468ab4d339e1ad7ad8ce2e035b5b8e2274396976
edbsousa <edeilson.sousa@hotmail.com>
2021-11-29 19:31:12 -0300

    Update format

--- mundo-1/ex008.py
+++ mundo-1/ex008.py
@@ -6,5 +6,5 @@ msg = "CONVERSOR DE MEDIDAS"
 print('='*45 + f'\n{msg:^45}\n' + '='*45)
 metros = float(input(f'\nDigite o valor em metros a ser convertido: '))
 
-print(f'\n- O valor {metros} em centímetros é {metros * 100}')
-print(f'- O valor {metros} em milímetros é {metros * 1000}')
+print(f'\n- O valor {metros}m em centímetros é {metros * 100:.0f}cm')
+print(f'- O valor {metros}m em milímetros é {metros * 1000:.0f}mm')
