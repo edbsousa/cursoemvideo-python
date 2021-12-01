7e48cff198956186ce9d07b26c715fe3ab4ff0f2
edbsousa <edeilson.sousa@hotmail.com>
2021-11-30 21:26:26 -0300

    Update: Converting float input  to radians

--- mundo-1/Usando_modulos_do_Python/ex018.py
+++ mundo-1/Usando_modulos_do_Python/ex018.py
@@ -1,10 +1,10 @@
 # Exercício 18 – Seno, Cosseno e Tangente
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 
-angulo = int(input('Digite o ângulo: '))
+angulo = float(input('Digite o ângulo: '))
 
-print(f'\nO valor do seno é: {sin(angulo)}. '
-      f'\nO valor do cosseno é {cos(angulo)}. '
-      f'\nE o valor da tangente é {tan(angulo)}.')
+print(f'\nO valor do seno é: {sin(radians(angulo)):.2f}. '
+      f'\nO valor do cosseno é {cos(radians(angulo)):.2f}. '
+      f'\nE o valor da tangente é {tan(radians(angulo)):.2f}.')
