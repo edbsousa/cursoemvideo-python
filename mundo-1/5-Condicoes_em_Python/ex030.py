546d29a28b1ca73ac79a0fd8f33a185f212c5d72
edbsousa <edeilson.sousa@hotmail.com>
2021-12-07 22:35:41 -0300

    Created 30 exercise

---
+++ mundo-1/5-Condicoes_em_Python/ex030.py
@@ -0,0 +1,10 @@
+# Exercício 30 – Par ou Ímpar?
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+num = int(input('Digite um número inteiro: '))
+
+
+if num % 2 == 0:
+    print('O número digitado é par!')
+else:
+    print('O número digitado é ímpar!')
