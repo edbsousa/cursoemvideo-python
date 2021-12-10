2966050b3ce0555ed0178a8bafc9c5bf11240cda
edbsousa <edeilson.sousa@hotmail.com>
2021-12-09 21:29:15 -0300

    Created 32 exercise

--- mundo-1/5-Condicoes_em_Python/ex032.py
+++ mundo-1/5-Condicoes_em_Python/ex032.py
@@ -1,8 +1,15 @@
 # Exercício 32 – Ano Bissexto
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 
-#ano = int(input('Digite o ano: '))
+ano = int(input('Digite o ano: '))
 
-ano = 2020
-
-print(ano)
+if ano % 4 == 0:
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print('Ano bissexto')
+        else:
+            print('Não é um ano bissexto')
+    else:
+        print('Ano bissexto')
+else:
+    print('Não é um ano bissexto')
