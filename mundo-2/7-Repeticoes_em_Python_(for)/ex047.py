f9bd7ca29d85f34262da63bc06f0331eddc96d81
edbsousa <edeilson.sousa@hotmail.com>
2021-12-19 22:52:53 -0300

    Created 47 exercise

---
+++ mundo-2/7-Repeticoes_em_Python_(for)/ex047.py
@@ -0,0 +1,7 @@
+# Exercício Python 47 - Contagem de pares
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+print('Os números pares entre 1 a 50 são:')
+
+for i in range(2, 51, 2):
+    print(i, end=' ')
