72af59230d5218d9aa83415de7ff59b75670eb24
edbsousa <edeilson.sousa@hotmail.com>
2022-01-18 23:04:56 -0300

    Created 83 exercise

---
+++ mundo-3/11-Listas/ex083.py
@@ -0,0 +1,24 @@
+# Exercício Python 83 - Validando expressões matemáticas
+# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+# Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+pilha = []
+expressao = str(input('Digite sua expressão: '))
+for simbolo in expressao:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão válida!')
+else:
+    print('Expressão inválida!')
+
+
+
+
+
